4b781c4615671d8ceb0ce918baa522c247357411
Marco Pivetta <ocramius@gmail.com>
2011-11-26 19:46:13 +0100

    Adding descriptive docbook

--- src/ZfPhpcrOdmSample/DocumentRepository/Greeting.php
+++ src/ZfPhpcrOdmSample/DocumentRepository/Greeting.php
@@ -4,6 +4,11 @@ namespace ZfPhpcrOdmSample\DocumentRepository;
 use Doctrine\ODM\PHPCR\Id\RepositoryIdInterface,
     Doctrine\ODM\PHPCR\DocumentRepository as BaseDocumentRepository;
 
+/**
+ * Simple repository used by PHPCR to generate ids for the "Greeting" document
+ * 
+ * @author Marco Pivetta <ocramius@gmail.com>
+ */
 class Greeting extends BaseDocumentRepository implements RepositoryIdInterface
 {
     /**
