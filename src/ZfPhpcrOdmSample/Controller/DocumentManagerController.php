78d8a24033656e35694cb62c53b6c6bd03a19969
Marco Pivetta <ocramius@gmail.com>
2011-11-26 11:08:02 +0100

    Creating simple controller and document to show the module functionality

---
+++ src/ZfPhpcrOdmSample/Controller/DocumentManagerController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace ZfPhpcrOdmSample\Controller;
+
+use Zend\Mvc\Controller\ActionController,
+    Doctrine\ODM\PHPCR\DocumentManager,
+    ZfPhpcrOdmSample\Document\Greeting;
+
+/**
+ * Sample controller used to show basic documentManager functionality
+ * 
+ * @author Marco Pivetta <ocramius@gmail.com>
+ */
+class DocumentManagerController extends ActionController
+{
+    
+    /**
+     * 
+     * @var DocumentManager 
+     */
+    protected $documentManager;
+    
+    /**
+     *
+     * @param DocumentManager $documentManager 
+     */
+    public function __construct(DocumentManager $documentManager) {
+        $this->documentManager = $documentManager;
+    }
+    
+    public function indexAction()
+    {
+        return array('documentManager' => $this->documentManager);
+    }
+    
+    public function persistGreetingAction() {
+        $greeting = new Greeting();
+        $greeting->setContent('Hello World!');
+        $this->documentManager->persist($greeting);
+        $this->documentManager->flush();
+        return array('greeting' => $greeting);
+    }
+    
+    public function listGreetingsAction() {
+        $greetings = $this
+            ->documentManager
+            ->getRepository('ZfPhpcrOdmSample\Document\Greeting')
+            ->findAll();
+        return array('greetings' => $greetings);
+    }
+    
+}
---
+++ src/ZfPhpcrOdmSample/Document/Greeting.php
@@ -0,0 +1,48 @@
+<?php
+namespace ZfPhpcrOdmSample\Document;
+
+use Doctrine\ODM\PHPCR\Mapping\Annotations as ODM;
+
+/**
+ * 
+ * Sample Document used to demonstrate DocumentManager functionality
+ * 
+ * @author Marco Pivetta <ocramius@gmail.com>
+ * 
+ * @ODM\Document
+ */
+class Greeting
+{
+    
+    /**
+     * @ODM\Id
+     */
+    private $id;
+
+    /**
+     * @ODM\String
+     */
+    private $content;
+    
+    /**
+     * @return string|null
+     */
+    public function getId() {
+        return $this->id;
+    }
+    
+    /**
+     * @return string|null
+     */
+    public function getContent() {
+        return $this->content;
+    }
+    
+    /**
+     * @param string $content 
+     */
+    public function setContent($content) {
+        $this->content = (string) $content;
+    }
+    
+}
