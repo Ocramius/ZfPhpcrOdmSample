e66c93a4001bc057931468952b40470f3b81da9d
Marco Pivetta <ocramius@gmail.com>
2012-04-08 07:43:40 +0200

    Moving directories to new convention (singular). Fixing view path

--- Module.php
+++ Module.php
@@ -2,29 +2,28 @@
 namespace ZfPhpcrOdmSample;
 
 /**
- * 
+ *
  * @author Marco Pivetta <ocramius@gmail.com>
  */
 class Module
 {
-    
     public function init()
     {
         $this->initAutoloader();
     }
-    
+
     public function initAutoloader()
     {
         require __DIR__ . '/autoload_register.php';
     }
-    
+
     /**
-     * 
+     *
      * @return Config
      */
     public function getConfig()
     {
-        return include __DIR__ . '/configs/module.config.php';
+        return include __DIR__ . '/config/module.config.php';
     }
-    
+
 }
